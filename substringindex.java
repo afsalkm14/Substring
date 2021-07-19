f5d2f7e316c9181c5441d719d11ec6aec5d5384c
afsalkm14 <63149776+afsalkm14@users.noreply.github.com>
2021-07-19 16:05:27 +0530

    Add files via upload

---
+++ substringindex.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+import java.util.Scanner;
+
+public class substringindex
+{
+
+	
+	static void findSubstring(String str, int power)
+	{
+
+		// Create an empty map
+		HashMap<Integer,
+				Integer> map = new HashMap<>();
+
+		
+		int curr_power = 0;
+		int len = str.length();
+
+		for (int i = 0; i < len; i++)
+		{
+
+			
+			curr_power = curr_power + (str.charAt(i) - 'a' + 1);
+
+			
+			if (curr_power == power)
+			{
+				System.out.println("Substring from index 0" +
+								" to " + i + " has power " + power);
+				System.out.println(str.substring(0, i+1));
+				return;
+			}
+
+			
+			if (map.containsKey(curr_power - power))
+			{
+				System.out.println("Substring from index " +
+								(map.get(curr_power - power) + 1) +
+								" to " + i + " has power " + power);
+				System.out.println(str.substring((map.get(curr_power - power)+1 ), i+1));
+				return;
+			}
+
+			map.put(curr_power, i);
+		}
+
+		
+		System.out.println("No substring with given power exists.");
+	}
+
+	
+	public static void main(String[] args)
+	{
+	    Scanner myObj = new Scanner(System.in);  
+	    Scanner myObj1 = new Scanner(System.in);  
+        System.out.println("Enter String");
+        String str = myObj.nextLine();  
+        System.out.println("Enter Power");
+        int power = myObj1.nextInt(); 
+
+		findSubstring(str, power);
+	}
+}
+
+
