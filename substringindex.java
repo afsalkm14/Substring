fec4d7cb4d769b27b7c7e83a70bcf0237334d348
afsalkm14 <63149776+afsalkm14@users.noreply.github.com>
2021-07-20 14:32:52 +0530

    Update substringindex.java

--- substringindex.java
+++ substringindex.java
@@ -20,10 +20,7 @@ public class substringindex
 		for (int i = 0; i < len; i++)
 		{
 
-			
 			curr_power = curr_power + (str.charAt(i) - 'a' + 1);
-
-			
 			if (curr_power == power)
 			{
 				System.out.println("Substring from index 0" +
@@ -32,7 +29,6 @@ public class substringindex
 				return;
 			}
 
-			
 			if (map.containsKey(curr_power - power))
 			{
 				System.out.println("Substring from index " +
@@ -45,21 +41,19 @@ public class substringindex
 			map.put(curr_power, i);
 		}
 
-		
 		System.out.println("No substring with given power exists.");
 	}
 
 	
 	public static void main(String[] args)
 	{
-	    Scanner myObj = new Scanner(System.in);  
-	    Scanner myObj1 = new Scanner(System.in);  
-        System.out.println("Enter String");
-        String str = myObj.nextLine();  
-        System.out.println("Enter Power");
-        int power = myObj1.nextInt(); 
-
-		findSubstring(str, power);
+	    Scanner sc = new Scanner(System.in);  
+	    Scanner sc = new Scanner(System.in);  
+            System.out.println("Enter String");
+            String str = sc.nextLine();  
+            System.out.println("Enter Power");
+            int power = sc.nextInt(); 
+	    findSubstring(str, power);
 	}
 }
 
